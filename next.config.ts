412e129004659fa3d9ebc3642fad9502dace34c9
Pablo Monardo <pablomonardo@gmail.com>
2025-05-17 17:04:14 -0300

    Agrego más archivitos

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['en', 'es'],
-    defaultLocale: 'es',
-    localeDetection: false,
-  }
+const nextConfig: NextConfig = {};
 
-};
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
 
-export default nextConfig;
